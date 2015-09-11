fea2e7102bb5307803a9cf3d1f540898210f2d3e
Shkodran Gerguri <shkodran.gerguri@mixrad.io>
2015-09-11 16:24:10 +0100

    prepare for initial release

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject sgerguri/clj-bucket "0.1.0-SNAPSHOT"
-  :description "A low-level implementation of the Token Bucket algorithm"
-  :url "http://github.com/sgerguri/clj-bucket"
+(defproject sgerguri/clj-bucket "0.1.0"
+  :description "A low-level implementation of the token bucket algorithm"
+  :url "https://github.com/sgerguri/clj-bucket"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :scm {:name "git"
+        :url "https://github.com/sgerguri/clj-bucket"})
